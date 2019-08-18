56ef51e9ccb087ca606f7adca4a48f5ac669ed2e
HrishiMane <HrishiMane@github.com>
2019-08-18 16:59:56 -0400

    Update

---
+++ SortingComparator/Solution.java
@@ -0,0 +1,53 @@
+package SortingComparator;
+
+import java.util.*;
+
+// Problem Statement: https://www.hackerrank.com/challenges/ctci-comparator-sorting/problem?h_l=interview&isFullScreen=true&playlist_slugs%5B%5D%5B%5D%5B%5D%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D%5B%5D%5B%5D%5B%5D=sorting
+
+class Player {
+    String name;
+    int score;
+
+    Player(String name, int score) {
+        this.name = name;
+        this.score = score;
+    }
+}
+
+class Checker implements Comparator<Player> {
+    // complete this method
+    public int compare(Player a, Player b) {
+        // for comparison
+        int nameComparator = a.name.compareTo(b.name);
+        int scoreComparator = a.score - b.score;
+
+        // 2 Level Comparision in " if - else "
+        if (scoreComparator == 0){
+            return nameComparator;
+        }else {
+            return scoreComparator * -1;
+        }
+    }
+}
+
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+
+        Player[] player = new Player[n];
+        Checker checker = new Checker();
+
+        for(int i = 0; i < n; i++){
+            player[i] = new Player(scan.next(), scan.nextInt());
+        }
+        scan.close();
+
+        Arrays.sort(player, checker);
+        for(int i = 0; i < player.length; i++){
+            System.out.printf("%s %s\n", player[i].name, player[i].score);
+        }
+    }
+}
