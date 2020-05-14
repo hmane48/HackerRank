6d5042f65d5f09cea4eb7cfa0724f7001472e2d9
Hrishikesh Mane <hmane480@gmail.com>
2020-05-14 01:14:36 -0400

    Update 20200514

--- CTCI/StacksAndQueues/MyQueue.java
+++ CTCI/StacksAndQueues/MyQueue.java
@@ -6,10 +6,10 @@ package CTCI.StacksAndQueues;
 
 import java.util.Stack;
 
-public class MyQueue {
+public class MyQueue<T> {
 
-    Stack<Integer> stack1;
-    Stack<Integer> stack2;
+    Stack<T> stack1;
+    Stack<T> stack2;
     int size;
 
     public MyQueue() {
@@ -18,22 +18,22 @@ public class MyQueue {
         size = 0;
     }
 
-    public void push(int value) {
+    public void push(T value) {
         stack1.push(value);
         size++;
     }
 
-    public Integer remove() {
+    public T remove() {
         if (size == 0){
             System.out.println("EmptyQueueException..");
-            return 0;
+            return null;
         }else if (size == 1){
             return stack1.pop();
         }
         while (size > 1){
             stack2.push(stack1.pop());
         }
-        int value = stack1.pop();
+        T value = stack1.pop();
         while (!stack2.isEmpty()) {
             stack1.push(stack2.pop());
         }
